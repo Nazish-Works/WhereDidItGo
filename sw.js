40ce6768a9c8a2781e22d142e79baf1ed29fdc7f
Nazish-Works <naazworks23@gmail.com>
2026-05-30 15:27:54 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -3,13 +3,13 @@ const ASSETS = [
   '/',
   '/index.html',
   '/manifest.json',
-  '/icons/icon-72.png',
-  '/icons/icon-96.png',
-  '/icons/icon-192.png',
-  '/icons/icon-512.png',
-  '/icons/maskable-192.png',
-  '/icons/maskable-512.png',
-  '/icons/apple-touch-icon.png'
+  '/icon-72.png',
+  '/icon-96.png',
+  '/icon-192.png',
+  '/icon-512.png',
+  '/maskable-192.png',
+  '/maskable-512.png',
+  '/apple-touch-icon.png'
 ];
 
 self.addEventListener('install', e => {
